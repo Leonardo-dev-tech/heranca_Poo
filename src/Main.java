6bab95215b4aee389943c2a0209ccbf21df811a0
Leonardo-dev-tech <leonardogomes.estagiariotech@gmail.com>
2025-03-25 09:46:17 -0300

    commit 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main{
 public static void main(String[] args) {
     Veiculo v = new Veiculo(" chevrolet"," celta");
     Carro c = new Carro(" fiat", " pálio",  4);
+    Moto m = new Moto();
         System.out.println(v);
         System.out.println(c);
+        System.out.println(m);
 }}
